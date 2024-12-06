6fff66f2132d8981ca49436819a67d45ffa4af05
Powerostad <ahsh516.ah@gmail.com>
2024-12-06 19:11:55 +0330

    changing the format of django_urls for simple usage

--- tradeproject/urls.py
+++ tradeproject/urls.py
@@ -24,16 +24,15 @@ from apps.accounts.views import GoogleCallbackView
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.dashboard.urls', namespace='main')),
-    path("api/accounts/", include("apps.accounts.urls")),
-    path("api/chatbot/", include("apps.chatbot.urls")),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-
-    # Optional UI:
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    ]
+    path('api/', include([
+        path('dashboard/', include('apps.dashboard.urls', namespace='dashboard')),
+        path('accounts/', include('apps.accounts.urls'), namespace='accounts'),
+        path('chatbot/', include('apps.chatbot.urls'), namespace='chatbot'),
+        path('schema/', SpectacularAPIView.as_view(), name='schema'),
+        path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+        path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    ])),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
