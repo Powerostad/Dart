aaa945eef3f72ad0c513f41767f4874ca372bb0f
Powerostad <ahsh516.ah@gmail.com>
2024-12-18 01:20:18 +0330

    fix urls for swagger

--- tradeproject/urls.py
+++ tradeproject/urls.py
@@ -1,39 +1,30 @@
-"""
-URL configuration for tradeproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.views.static import serve
+
 from apps.accounts import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
-from apps.accounts.views import GoogleCallbackView
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+static_urlpatterns = [
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+]
+
+
 urlpatterns = [
     path('api/', include([
         path('dashboard/', include('apps.dashboard.urls', namespace='dashboard')),
         path('accounts/', include('apps.accounts.urls', namespace='accounts')),
         path('chatbot/', include('apps.chatbot.urls', namespace='chatbot')),
-        path('schema/', SpectacularAPIView.as_view(), name='schema'),
-        path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-        path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+        path('docs/', TemplateView.as_view(template_name="index.html"), name="swagger-ui"),
+
         path('blog/', include('apps.blog.urls', namespace='blog')),
 
     ])),
+    path("", include(static_urlpatterns)),
 ]
 
 if settings.DEBUG:
