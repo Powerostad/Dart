37a8cae590a29cf257c9dc6f90a90af50981f7b2
AmirHossein Sharifi Nejad <90046706+Powerostad@users.noreply.github.com>
2024-12-31 21:53:52 +0330

    Update settings.py

--- tradeproject/settings.py
+++ tradeproject/settings.py
@@ -261,7 +261,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
