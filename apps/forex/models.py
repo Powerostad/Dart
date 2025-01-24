7922f1e5e79aa94f90c92b2ec1c49aa5ed1bf10e
Powerostad <ahsh516.ah@gmail.com>
2025-01-24 18:28:36 +0330

    fix celery error for saving natural signal

--- apps/forex/models.py
+++ apps/forex/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Signal(models.Model):
     symbol = models.CharField(max_length=20)
     timeframe = models.CharField(max_length=3)
-    signal_type = models.CharField(max_length=4)
+    signal_type = models.CharField(max_length=10)
     confidence = models.DecimalField(max_digits=5, decimal_places=2)
     algorithms_triggered = models.JSONField()
 
